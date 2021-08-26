b90a06571d57f17bb57a55d97fff451f4c8ff3c5
Valentino Destefano <vdestefano@correo.unimet.edu.ve>
2021-08-26 18:40:35 -0400

    First version 1.0

--- lib/tabs/creator_tabs.dart
+++ lib/tabs/creator_tabs.dart
@@ -5,6 +5,38 @@ class CreatorTabs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text(
+            "Developed by",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontFamily: "Oswald", fontSize: 20, color: Color(0xFF900C3F)),
+          ),
+          Text(
+            "Valentino Destefano",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontFamily: "Oswald",
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF900C3F)),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(80),
+            ),
+            child: Container(
+              height: 250,
+              decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                      image: new AssetImage("assets/images/profile.jpeg"),
+                      fit: BoxFit.fitHeight)),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
